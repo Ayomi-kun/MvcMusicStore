f0cc97b6e195c59eaa9682fd229207e6d453bb18
Ayomikun Oluwayemi <oluwayemiayo@yahoo.com>
2018-04-17 15:19:37 +0100

    annotation attribute 'Display' for custom label rendering

--- MvcMusicStore/Models/Order.cs
+++ MvcMusicStore/Models/Order.cs
@@ -10,23 +10,29 @@ namespace MvcMusicStore.Models
     public class Order
     {
         public int OrderId { get; set; }
+        [Display(Name="Order Date")]
         public DateTime OrderDate { get; set; }
         [Remote ("CheckUserName", "Account")]
+        [Display(Name = "User Name")]
         public string Username { get; set; }
         [Required]
         [StringLength (160,MinimumLength=3)]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
         [Required]
         [StringLength (160,MinimumLength=3)]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
+        [Display(Name = "Postal Code")]
         public string PostalCode { get; set; }
         public string Country { get; set; }
         public string Phone { get; set; }
         [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+ \. [A-za-z]{2,4}", ErrorMessage="{0} does not look like a valid {0} address.")]
         public string Email { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode=true,DataFormatString="{0:c}")]
         public decimal Total { get; set; }
        // public List<OrderDetail> OrderDetail { get; set; }
     }
