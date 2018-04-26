c30017403ee11397739117864c89d3a2aaa3773f
Ayomikun Oluwayemi <oluwayemiayo@yahoo.com>
2018-04-26 22:04:58 +0100

    application js meme

---
+++ MvcMusicStore/Scripts/mvcmusicstore.js
@@ -0,0 +1,7 @@
+﻿//my very own js File
+$(function () {
+    $(".lead").hover(function () {
+        $(this).animate({ height: '+=25', width: '+=25' })
+               .animate({ height: '+=25', width: '+=25' });
+    });
+});
